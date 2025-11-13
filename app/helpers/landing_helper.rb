8ee925282fd6e38f3cd30afbd564a85868227424
bszarlowicz <b.szarlowicz@gmail.com>
2025-11-14 00:32:05 +0100

    [new] landing adjustments

--- app/helpers/landing_helper.rb
+++ app/helpers/landing_helper.rb
@@ -22,12 +22,12 @@ module LandingHelper
 
   def mentora_icons
     [
-      { path: 'railsIcon.png', text: 'Rails', version: '7.0.7' },
-      { path: 'rubyIcon.png', text: 'Ruby', version: '3.1.2' },
+      { path: 'railsIcon.png', text: 'Rails', version: '7.1.3' },
+      { path: 'rubyIcon.png', text: 'Ruby', version: '3.3.2' },
       { path: 'javascriptIcon.png', text: 'JavaScript' },
-      { path: 'sqlIcon.png', text: 'sqlite3', version: '1.4' },
+      { path: 'mySqlIcon.png', text: 'MySQL', version: '0.5.4' },
       { path: 'cssIcon.png', text: 'CSS3' },
-      { path: 'bootstrapIcon.png', text: 'Bootstrap', version: '5.3.2' },
+      { path: 'bootstrapIcon.png', text: 'Bootstrap', version: '5.1.3' },
       { path: 'htmlIcon.png', text: 'HTML5' }
     ]
   end
@@ -55,8 +55,9 @@ module LandingHelper
 
   def mentora_links
     [
-      { icon: 'github', url: 'https://github.com/bszarlowicz/RegionHub', text: 'Checkout code!' },
-      { icon: 'file-pdf-o', url: '#', text: 'Checkout docs!' },
+      { icon: 'github', url: 'https://github.com/bszarlowicz/ClassInsight', text: 'Checkout code!' },
+      { icon: 'file-pdf-o', file: '/docs/mentora_docs.pdf', text: 'Checkout docs!' },
+      { icon: 'download', file: '/docs/mentora_presentation.pptx', text: 'Checkout presentation!' },
       { icon: 'globe', url: nil, text: 'Try it!', sub_text: 'Soon...' }
     ]
   end
@@ -64,7 +65,8 @@ module LandingHelper
   # ===== WAVE SVG PATTERNS =====
 
   def wave_patterns
-    mentora_top_svg = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none"><path d="M0,60 C300,90 500,30 600,60 C700,90 900,30 1200,60 L1200,120 L0,120 Z" fill="#47b278"/></svg>'
+    # Mentora - delikatne fale
+    mentora_top_svg = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none"><path d="M0,60 C200,40 400,40 600,60 C800,80 1000,80 1200,60 L1200,120 L0,120 Z" fill="#47b278"/></svg>'
     mentora_bottom_svg = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none"><path d="M0,50 C150,0 350,100 600,50 C850,0 1050,100 1200,50 L1200,0 L0,0 Z" fill="#47b278"/></svg>'
     
     {
@@ -79,9 +81,9 @@ module LandingHelper
 
   def mentora_project(selected_tab = 'about')
     {
-      project_id: 'Mentora',
+      project_id: 'mentora',
       project_title: 'Mentora',
-      project_subtitle: 'Demographic data of regions in Poland and the USA - extensive database with analysis',
+      project_subtitle: 'A Platform for Managing Extracurricular Learning (Engineering Thesis)',
       preview_images: mentora_preview_images,
       technologies: mentora_icons,
       tabs_data: mentora_data,
