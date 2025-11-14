3f061fd3de30cba2e8a1ae4940c55f1363aafa25
bszarlowicz <b.szarlowicz@gmail.com>
2025-11-14 15:24:17 +0100

    [new] login page config

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  require 'base64'
+
+  def login_wave_pattern
+    wave_svg = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none"><path d="M0,60 C200,40 400,40 600,60 C800,80 1000,80 1200,60 L1200,120 L0,120 Z" fill="#47b278"/></svg>'
+    
+    "data:image/svg+xml;base64,#{Base64.strict_encode64(wave_svg)}"
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module Omen
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
-    config.i18n.default_locale = :pl
+    config.i18n.default_locale = :en
 
     # Configuration for the application, engines, and railties goes here.
     #
